1cdcfb65d5426145f0f69240aaac78dcd430b00c
Leonardo Araujo dos Santos <leonardoaraujo.santos@gmail.com>
2016-06-06 11:59:20 +0100

    Improving code comments, and start to work with tables

--- simple/callLuaFromC.c
+++ simple/callLuaFromC.c
@@ -1,6 +1,7 @@
 /*Call lua from C
 Tutorials and Reference:
 http://www.troubleshooters.com/codecorn/lua/lua_c_calls_lua.htm
+https://john.nachtimwald.com/2014/07/26/calling-lua-from-c/
 
 Installing lua 5.1
 sudo apt-get install lua5.1
@@ -8,6 +9,9 @@ sudo apt-get install liblua5.1-0-dev
 
 Compiling
 gcc callLuaFromC.c -o callLuaFromC -I/usr/include/lua5.1 -llua5.1
+
+Future notes: Add this arround a mex file and see how to send/receive tables
+lua arrays
 */
 #include <stdio.h>
 
@@ -22,6 +26,12 @@ void luaError(lua_State *L, char *msg)
 {
   fprintf(stderr, "\nFATAL ERROR:\n  %s: %s\n\n",msg, lua_tostring(L, -1));
   exit(1);
+}
+
+// Used to push an array of integers
+void pushIntegetArray()
+{
+
 }
 
 int main(void)
@@ -44,14 +54,16 @@ int main(void)
     luaError(L, "lua_pcall() failed");
 
   // Indicate function that will be called (Push function to lua stack)
+  printf("SAY SOMETHING ###################################################\n");
   lua_getglobal(L, "saySomething");
 
-  // Call the function
+  // Call the function (No arguments, no return value)
   if (lua_pcall(L, 0, 0, 0))
     luaError(L, "lua_pcall() failed");
 
   // Call function with one argument and one output
   // Push to stack function name
+  printf("SQUARE ###################################################\n");
   lua_getglobal(L, "square");
 
   // Push to stack first argument
@@ -68,6 +80,7 @@ int main(void)
 
   // Call function with two argument and one output
   // Push to stack function name
+  printf("ADD SOMETHING ###################################################\n");
   lua_getglobal(L, "addSomething");
 
   // Push to stack 2 arguments
@@ -81,6 +94,34 @@ int main(void)
   // Gather result (pop one item from stack)
   resultLua = lua_tonumber(L, -1);
   printf("Returned number for lua_addSomething(6,4)=%d\n",resultLua);
+
+  printf("SEND ARAY ###################################################\n");
+  printf("In C, calling Lua->tweaktable()\n");
+  lua_getglobal(L, "tweaktable");             /* Tell it to run callfuncscript.lua->tweaktable() */
+  lua_newtable(L);                            /* Push empty table onto stack table now at -1 */
+  lua_pushliteral(L, "fname");                /* Push a key onto the stack, table now at -2 */
+  lua_pushliteral(L, "Margie");               /* Push a value onto the stack, table now at -3 */
+  lua_settable(L, -3);                        /* Take key and value, put into table at -3, */
+                                              /*  then pop key and value so table again at -1 */
+
+  lua_pushliteral(L, "lname");                /* Push a key onto the stack, table now at -2 */
+  lua_pushliteral(L, "Martinez");             /* Push a value onto the stack, table now at -3 */
+  lua_settable(L, -3);                        /* Take key and value, put into table at -3, */
+                                              /*  then pop key and value so table again at -1 */
+
+  if (lua_pcall(L, 1, 1, 0))                  /* Run function, !!! NRETURN=1 !!! */
+    luaError(L, "lua_pcall() failed");
+
+  lua_pushnil(L);
+  const char *k, *v;
+    while (lua_next(L, -2)) {                    /* TABLE LOCATED AT -2 IN STACK */
+        v = lua_tostring(L, -1);                 /* Value at stacktop */
+        lua_pop(L,1);                            /* Remove value */
+        k = lua_tostring(L, -1);                 /* Read key at stacktop, */
+                                                 /* leave in place to guide next lua_next() */
+        printf("Fromc k=>%s<, v=>%s<\n", k, v);
+    }
+
 
   printf("Back in C again\n");
 
--- simple/someLuaFuncs.lua
+++ simple/someLuaFuncs.lua
@@ -1,7 +1,9 @@
+-- Simple function without argument and return value
 function saySomething()
 	io.write("Some message from Lua.\n")
 end
 
+-- Function with 1 argument and 1 return value
 function square(n)
 	io.write("Within callfuncscript.lua fcn square, arg=")
 	io.write(tostring(n))
@@ -12,9 +14,39 @@ function square(n)
 	return(n)
 end
 
+-- Function with 2 arguments and 1 return value
 function addSomething(a,b)
 	io.write('Add something')
 	return (a+b)
 end
 
-print("Priming run")
+
+-- Function with table argument
+function showTableContent(tab_in)
+	-- Iterate on lua table
+	for idx,val in pairs(tab_in) do
+		io.write(string.format("idx=%d val=%d\n",idx,val))
+	end
+end
+
+-- Hijacked from tutorial remove later
+function tweaktable(tab_in)
+	local tab_out = {numfields=1}
+	for k,v in pairs(tab_in) do
+		tab_out.numfields = tab_out.numfields + 1
+		tab_out[tostring(k)] = string.upper(tostring(v))
+	end
+	tab_out.numfields = tostring(tab_out.numfields)
+	io.write("At bottom of callfuncscript.lua tweaktable(), numfields=")
+	io.write(tab_out.numfields)
+	print()
+	return tab_out
+end
+
+print("Priming run (Needed before calling functions)")
+--b = {};
+--b[1] = 0;
+--b[2] = 10;
+--b[3] = 20;
+b = {0,10,20,30}
+showTableContent(b)
